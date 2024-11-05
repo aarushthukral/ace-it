e7d0f1333d4e4097531a126c6cfbb83a1b6c3e84
aarushthukral <81401731+aarushthukral@users.noreply.github.com>
2024-11-05 08:17:44 +0100

    formatting changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,15 +3,13 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 
 const config: Config = {
   darkMode: "class",
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
-    }
-  }
+    },
+  },
 };
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,47 @@
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
-import fs from 'fs';
-import { defineConfig } from 'vite';
-import { crx, ManifestV3Export } from '@crxjs/vite-plugin';
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import fs from "fs";
+import { defineConfig } from "vite";
+import { crx, ManifestV3Export } from "@crxjs/vite-plugin";
 
-import manifest from './manifest.json';
-import devManifest from './manifest.dev.json';
-import pkg from './package.json';
+import manifest from "./manifest.json";
+import devManifest from "./manifest.dev.json";
+import pkg from "./package.json";
 
-const root = resolve(__dirname, 'src');
-const pagesDir = resolve(root, 'pages');
-const assetsDir = resolve(root, 'assets');
-const outDir = resolve(__dirname, 'dist');
-const publicDir = resolve(__dirname, 'public');
+const root = resolve(__dirname, "src");
+const pagesDir = resolve(root, "pages");
+const assetsDir = resolve(root, "assets");
+const outDir = resolve(__dirname, "dist");
+const publicDir = resolve(__dirname, "public");
 
-const isDev = process.env.__DEV__ === 'true';
+const isDev = process.env.__DEV__ === "true";
 
 const extensionManifest = {
   ...manifest,
-  ...(isDev ? devManifest : {} as ManifestV3Export),
-  name: isDev ? `DEV: ${ manifest.name }` : manifest.name,
+  ...(isDev ? devManifest : ({} as ManifestV3Export)),
+  name: isDev ? `DEV: ${manifest.name}` : manifest.name,
   version: pkg.version,
 };
 
 // plugin to remove dev icons from prod build
-function stripDevIcons (isDev: boolean) {
-  if (isDev) return null
+function stripDevIcons(isDev: boolean) {
+  if (isDev) return null;
 
   return {
-    name: 'strip-dev-icons',
-    resolveId (source: string) {
-      return source === 'virtual-module' ? source : null
+    name: "strip-dev-icons",
+    resolveId(source: string) {
+      return source === "virtual-module" ? source : null;
     },
-    renderStart (outputOptions: any, inputOptions: any) {
-      const outDir = outputOptions.dir
-      fs.rm(resolve(outDir, 'dev-icon-32.png'), () => console.log(`Deleted dev-icon-32.png from prod build`))
-      fs.rm(resolve(outDir, 'dev-icon-128.png'), () => console.log(`Deleted dev-icon-128.png from prod build`))
-    }
-  }
+    renderStart(outputOptions: any, inputOptions: any) {
+      const outDir = outputOptions.dir;
+      fs.rm(resolve(outDir, "dev-icon-32.png"), () =>
+        console.log(`Deleted dev-icon-32.png from prod build`),
+      );
+      fs.rm(resolve(outDir, "dev-icon-128.png"), () =>
+        console.log(`Deleted dev-icon-128.png from prod build`),
+      );
+    },
+  };
 }
 
 /*
@@ -62,26 +66,26 @@ function stripDevIcons (isDev: boolean) {
 export default defineConfig({
   resolve: {
     alias: {
-      '@src': root,
-      '@assets': assetsDir,
-      '@pages': pagesDir,
+      "@src": root,
+      "@assets": assetsDir,
+      "@pages": pagesDir,
     },
   },
   plugins: [
     react(),
     crx({
       manifest: extensionManifest as ManifestV3Export,
-      browser: 'chrome', // <-- change value to 'firefox' or 'chrome'
+      browser: "chrome", // <-- change value to 'firefox' or 'chrome'
       contentScripts: {
         injectCss: true,
-      }
+      },
     }),
-    stripDevIcons(isDev)
+    stripDevIcons(isDev),
   ],
   publicDir,
   build: {
     outDir,
     sourcemap: isDev,
-    emptyOutDir: !isDev
+    emptyOutDir: !isDev,
   },
 });
